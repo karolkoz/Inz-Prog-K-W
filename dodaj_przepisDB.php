b42f4e1144da26c50fb6cab02e3720f9c91f64b2
mwiech99 <80545933+mwiech99@users.noreply.github.com>
2021-04-15 18:37:34 +0200

    dodaj_przepisDB
    propel

--- dodaj_przepisDB.php
+++ dodaj_przepisDB.php
@@ -7,35 +7,117 @@ $trudnosc = $_POST['trudnosc'];
 $czas_przygotowania = $_POST['czas_przygotowania'];
 $ile_osob = $_POST['ile_osob'];
 $opis = $_POST['opis'];
-////ponizsze sie wstawiaja, tych wyzej jeszcze nie - do naprawy
 $data=date("Y-m-d");
 $status = 1;
 $UZYTKOWNIK_login = "dummy123";
 
 
-
+/////////////////dodawanie do tabeli przepis/////////////////////////////
 $przepis = new Przepis();
 
 
 $przepis->setNazwa($nazwa);
 $przepis->setStopienTrudnosci($trudnosc);
 $przepis->setCzasPrzygotowania($czas_przygotowania);
+$przepis->setDlaIluOsob($ile_osob);
 $przepis->setDataDodania($data);
 $przepis->setStatus($status);
 $przepis->setUzytkownikLogin($UZYTKOWNIK_login);
 
-// $przepis->setNazwa('nalesniki');
-// $przepis->setStopienTrudnosci(2);
-// $przepis->setCzasPrzygotowania(15);
-// $przepis->setDataDodania($data);
-// $przepis->setStatus(1);
-// $przepis->setUzytkownikLogin('dummy123');
-
-
-
 if($przepis->save())
 {
     echo 'Dodano przepis o id: '.$przepis->getIdPrzepis();
+    $idPrzepisu=$przepis->getIdPrzepis();
 }
+
+/////////////////////dodawanie do tabeli skladniki i zawiera ////////////////////////////
+$tab=[];
+$i=0;
+foreach($_POST['skladnik_ilosc'] as $val)
+{
+  $tab[$i]=$val;
+  $i++;
+} //mam tablice tab o elementach z tablicy $_POST['skladnik_ilosc']
+
+
+  $j=0;
+  foreach ($_POST['skladnik_nazwa'] as $value)
+  {
+    $skladnik = new Skladniki();
+    $skladnik->setNazwa($value);
+    $skladnik->save();
+
+    $zawiera = new Zawiera();
+    $zawiera->setPrzepis($przepis);
+    $zawiera->setSkladniki($skladnik);
+    $zawiera->setIlosc($tab[$j]);
+    $zawiera->save();
+
+    $j++;
+    // echo " Wstawiono do tabaeli zawiera! ";
+  }
+
+
+
+  ///////////////////////////////////////dodawanie etapów////////////////////////
+  $num = count($_POST['etap']); //ilosc dodanych opisow etapow
+
+  $nr_etap=1; //zaczynamy od etapu nr 1, bedziemy zwiekszac $nr_etap++ przy dodawaniu kolejnych etapow
+
+  $tab2=[];
+  $k=0;
+  foreach($_POST['etap'] as $val_opis)
+  {
+    $tab2[$k]=$val_opis;
+    $k++;
+  }//mam tablice tab2 o elementach z tablicy $_POST['etap'] czyli opis etapu
+  //np. dla 3 etapow tablica ma 4 elementy
+
+
+  if($num==2)
+  {
+    $etap = new Etap();
+    $etap->setNrEtapu($nr_etap);
+    $etap->setOpis($tab2[0]);
+    $etap->setPrzepis($przepis);
+    $etap->save();
+
+  }
+
+  $n=2;
+  if($num>2)
+  {
+    $etap = new Etap();
+    $etap->setNrEtapu($nr_etap);
+    $etap->setOpis($tab2[0]);
+    $etap->setPrzepis($przepis);
+    $etap->save();
+
+    $nr_etap++;
+    for($m=2; $m<$num; $m++)
+    {
+      $etap = new Etap();
+      $etap->setNrEtapu($nr_etap);
+      $etap->setOpis($tab2[$n]);
+      $etap->setPrzepis($przepis);
+      $etap->save();
+      $n++;
+      $nr_etap++;
+    }
+  }
+
+
+  //////////////////////////////////////////dodawanie kategorii
+
+  foreach ($_POST['categories'] as $name_categories)
+  {
+    $nalez = new Nalezy();
+    $nalez->setPrzepis($przepis);
+    $nalez->setKategoriaNazwa($name_categories);
+    $nalez->save();
+    // echo "  dodano kategorię ";
+  }
+
+
 
 ?>
