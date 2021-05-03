84714c70a38f38e0c04f07b09ed299ff610bf338
Karol Kozłowski <githaslo1>
2021-05-03 16:51:39 +0200

    wyswietl i edytuj przepis
    Lepsze wyświetlanie na większych ekranach i poprawiona funkcja w edytuj przepis

--- edytuj_przepis.php
+++ edytuj_przepis.php
@@ -1,8 +1,6 @@
 <html>
 
 <head>
-  <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
-    crossorigin="anonymous"> -->
   <link rel="Stylesheet" type="text/css" href="style/css/style.css" />
   <meta charset="utf-8" />
   <title>Pyszniutkie.pl</title>
@@ -27,8 +25,6 @@
 
           echo '<input type="text" id="nazwa" name="nazwa" placeholder="Nazwa" value="'.$przepis->getNazwa().'" onchange="name_validation()" required />'
 
-          // $nazwa = "Jakas nazwa";
-          // echo '<input type="text" id="nazwa" name="nazwa" placeholder="Nazwa" value="'.$nazwa.'" />'
           ?>
         </div>
         <div class="content__form__input">
@@ -46,241 +42,15 @@
 
             $przepis = PrzepisQuery::create()->findPk($id_przepis);
             //echo $przepis->getStopienTrudnosci();
-
-            if($przepis->getStopienTrudnosci()==1){
-              echo '<input type="radio" id="trudnosc10" name="trudnosc" value="10" />
-              <label for="trudnosc10"></label>
-              <input type="radio" id="trudnosc9" name="trudnosc" value="9" />
-              <label for="trudnosc9"></label>
-              <input type="radio" id="trudnosc8" name="trudnosc" value="8" />
-              <label for="trudnosc8"></label>
-              <input type="radio" id="trudnosc7" name="trudnosc" value="7" />
-              <label for="trudnosc7"></label>
-              <input type="radio" id="trudnosc6" name="trudnosc" value="6"  />
-              <label for="trudnosc6"></label>
-              <input type="radio" id="trudnosc5" name="trudnosc" value="5" />
-              <label for="trudnosc5"></label>
-              <input type="radio" id="trudnosc4" name="trudnosc" value="4" />
-              <label for="trudnosc4"></label>
-              <input type="radio" id="trudnosc3" name="trudnosc" value="3" />
-              <label for="trudnosc3"></label>
-              <input type="radio" id="trudnosc2" name="trudnosc" value="2" />
-              <label for="trudnosc2"></label>
-              <input type="radio" id="trudnosc1" name="trudnosc" value="1" checked  />
-              <label for="trudnosc1"></label>';
+            for($i=10; $i>=1; $i--) {
+              if($przepis->getStopienTrudnosci()==$i) {
+                echo '<input type="radio" id="trudnosc'.$i.'" name="trudnosc" value="'.$i.'" checked />
+                <label for="trudnosc'.$i.'"></label>';
+              } else {
+                echo '<input type="radio" id="trudnosc'.$i.'" name="trudnosc" value="'.$i.'" />
+                <label for="trudnosc'.$i.'"></label>';
+              }
             }
-
-            if($przepis->getStopienTrudnosci()==2){
-              echo '<input type="radio" id="trudnosc10" name="trudnosc" value="10" />
-              <label for="trudnosc10"></label>
-              <input type="radio" id="trudnosc9" name="trudnosc" value="9" />
-              <label for="trudnosc9"></label>
-              <input type="radio" id="trudnosc8" name="trudnosc" value="8" />
-              <label for="trudnosc8"></label>
-              <input type="radio" id="trudnosc7" name="trudnosc" value="7" />
-              <label for="trudnosc7"></label>
-              <input type="radio" id="trudnosc6" name="trudnosc" value="6"  />
-              <label for="trudnosc6"></label>
-              <input type="radio" id="trudnosc5" name="trudnosc" value="5" />
-              <label for="trudnosc5"></label>
-              <input type="radio" id="trudnosc4" name="trudnosc" value="4" />
-              <label for="trudnosc4"></label>
-              <input type="radio" id="trudnosc3" name="trudnosc" value="3" />
-              <label for="trudnosc3"></label>
-              <input type="radio" id="trudnosc2" name="trudnosc" value="2" checked />
-              <label for="trudnosc2"></label>
-              <input type="radio" id="trudnosc1" name="trudnosc" value="1" />
-              <label for="trudnosc1"></label>';
-            }
-
-
-
-            if($przepis->getStopienTrudnosci()==3){
-              echo '<input type="radio" id="trudnosc10" name="trudnosc" value="10" />
-              <label for="trudnosc10"></label>
-              <input type="radio" id="trudnosc9" name="trudnosc" value="9" />
-              <label for="trudnosc9"></label>
-              <input type="radio" id="trudnosc8" name="trudnosc" value="8" />
-              <label for="trudnosc8"></label>
-              <input type="radio" id="trudnosc7" name="trudnosc" value="7" />
-              <label for="trudnosc7"></label>
-              <input type="radio" id="trudnosc6" name="trudnosc" value="6"  />
-              <label for="trudnosc6"></label>
-              <input type="radio" id="trudnosc5" name="trudnosc" value="5" />
-              <label for="trudnosc5"></label>
-              <input type="radio" id="trudnosc4" name="trudnosc" value="4" />
-              <label for="trudnosc4"></label>
-              <input type="radio" id="trudnosc3" name="trudnosc" value="3"  checked />
-              <label for="trudnosc3"></label>
-              <input type="radio" id="trudnosc2" name="trudnosc" value="2"/>
-              <label for="trudnosc2"></label>
-              <input type="radio" id="trudnosc1" name="trudnosc" value="1" />
-              <label for="trudnosc1"></label>';
-            }
-
-            if($przepis->getStopienTrudnosci()==4){
-              echo '<input type="radio" id="trudnosc10" name="trudnosc" value="10" />
-              <label for="trudnosc10"></label>
-              <input type="radio" id="trudnosc9" name="trudnosc" value="9" />
-              <label for="trudnosc9"></label>
-              <input type="radio" id="trudnosc8" name="trudnosc" value="8" />
-              <label for="trudnosc8"></label>
-              <input type="radio" id="trudnosc7" name="trudnosc" value="7" />
-              <label for="trudnosc7"></label>
-              <input type="radio" id="trudnosc6" name="trudnosc" value="6"  />
-              <label for="trudnosc6"></label>
-              <input type="radio" id="trudnosc5" name="trudnosc" value="5" />
-              <label for="trudnosc5"></label>
-              <input type="radio" id="trudnosc4" name="trudnosc" value="4" checked />
-              <label for="trudnosc4"></label>
-              <input type="radio" id="trudnosc3" name="trudnosc" value="3" />
-              <label for="trudnosc3"></label>
-              <input type="radio" id="trudnosc2" name="trudnosc" value="2"/>
-              <label for="trudnosc2"></label>
-              <input type="radio" id="trudnosc1" name="trudnosc" value="1" />
-              <label for="trudnosc1"></label>';
-            }
-
-            if($przepis->getStopienTrudnosci()==5){
-              echo '<input type="radio" id="trudnosc10" name="trudnosc" value="10" />
-              <label for="trudnosc10"></label>
-              <input type="radio" id="trudnosc9" name="trudnosc" value="9" />
-              <label for="trudnosc9"></label>
-              <input type="radio" id="trudnosc8" name="trudnosc" value="8" />
-              <label for="trudnosc8"></label>
-              <input type="radio" id="trudnosc7" name="trudnosc" value="7" />
-              <label for="trudnosc7"></label>
-              <input type="radio" id="trudnosc6" name="trudnosc" value="6"  />
-              <label for="trudnosc6"></label>
-              <input type="radio" id="trudnosc5" name="trudnosc" value="5" checked />
-              <label for="trudnosc5"></label>
-              <input type="radio" id="trudnosc4" name="trudnosc" value="4" />
-              <label for="trudnosc4"></label>
-              <input type="radio" id="trudnosc3" name="trudnosc" value="3" />
-              <label for="trudnosc3"></label>
-              <input type="radio" id="trudnosc2" name="trudnosc" value="2"/>
-              <label for="trudnosc2"></label>
-              <input type="radio" id="trudnosc1" name="trudnosc" value="1" />
-              <label for="trudnosc1"></label>';
-            }
-
-            if($przepis->getStopienTrudnosci()==6){
-              echo '<input type="radio" id="trudnosc10" name="trudnosc" value="10" />
-              <label for="trudnosc10"></label>
-              <input type="radio" id="trudnosc9" name="trudnosc" value="9" />
-              <label for="trudnosc9"></label>
-              <input type="radio" id="trudnosc8" name="trudnosc" value="8" />
-              <label for="trudnosc8"></label>
-              <input type="radio" id="trudnosc7" name="trudnosc" value="7" />
-              <label for="trudnosc7"></label>
-              <input type="radio" id="trudnosc6" name="trudnosc" value="6" checked  />
-              <label for="trudnosc6"></label>
-              <input type="radio" id="trudnosc5" name="trudnosc" value="5" />
-              <label for="trudnosc5"></label>
-              <input type="radio" id="trudnosc4" name="trudnosc" value="4" />
-              <label for="trudnosc4"></label>
-              <input type="radio" id="trudnosc3" name="trudnosc" value="3" />
-              <label for="trudnosc3"></label>
-              <input type="radio" id="trudnosc2" name="trudnosc" value="2"/>
-              <label for="trudnosc2"></label>
-              <input type="radio" id="trudnosc1" name="trudnosc" value="1" />
-              <label for="trudnosc1"></label>';
-            }
-
-            if($przepis->getStopienTrudnosci()==7){
-              echo '<input type="radio" id="trudnosc10" name="trudnosc" value="10" />
-              <label for="trudnosc10"></label>
-              <input type="radio" id="trudnosc9" name="trudnosc" value="9" />
-              <label for="trudnosc9"></label>
-              <input type="radio" id="trudnosc8" name="trudnosc" value="8" />
-              <label for="trudnosc8"></label>
-              <input type="radio" id="trudnosc7" name="trudnosc" value="7" checked />
-              <label for="trudnosc7"></label>
-              <input type="radio" id="trudnosc6" name="trudnosc" value="6"  />
-              <label for="trudnosc6"></label>
-              <input type="radio" id="trudnosc5" name="trudnosc" value="5" />
-              <label for="trudnosc5"></label>
-              <input type="radio" id="trudnosc4" name="trudnosc" value="4" />
-              <label for="trudnosc4"></label>
-              <input type="radio" id="trudnosc3" name="trudnosc" value="3" />
-              <label for="trudnosc3"></label>
-              <input type="radio" id="trudnosc2" name="trudnosc" value="2"/>
-              <label for="trudnosc2"></label>
-              <input type="radio" id="trudnosc1" name="trudnosc" value="1" />
-              <label for="trudnosc1"></label>';
-            }
-
-            if($przepis->getStopienTrudnosci()==8){
-              echo '<input type="radio" id="trudnosc10" name="trudnosc" value="10" />
-              <label for="trudnosc10"></label>
-              <input type="radio" id="trudnosc9" name="trudnosc" value="9" />
-              <label for="trudnosc9"></label>
-              <input type="radio" id="trudnosc8" name="trudnosc" value="8" checked />
-              <label for="trudnosc8"></label>
-              <input type="radio" id="trudnosc7" name="trudnosc" value="7" />
-              <label for="trudnosc7"></label>
-              <input type="radio" id="trudnosc6" name="trudnosc" value="6"  />
-              <label for="trudnosc6"></label>
-              <input type="radio" id="trudnosc5" name="trudnosc" value="5" />
-              <label for="trudnosc5"></label>
-              <input type="radio" id="trudnosc4" name="trudnosc" value="4" />
-              <label for="trudnosc4"></label>
-              <input type="radio" id="trudnosc3" name="trudnosc" value="3" />
-              <label for="trudnosc3"></label>
-              <input type="radio" id="trudnosc2" name="trudnosc" value="2"/>
-              <label for="trudnosc2"></label>
-              <input type="radio" id="trudnosc1" name="trudnosc" value="1" />
-              <label for="trudnosc1"></label>';
-            }
-
-
-            if($przepis->getStopienTrudnosci()==9){
-              echo '<input type="radio" id="trudnosc10" name="trudnosc" value="10" />
-              <label for="trudnosc10"></label>
-              <input type="radio" id="trudnosc9" name="trudnosc" value="9" checked />
-              <label for="trudnosc9"></label>
-              <input type="radio" id="trudnosc8" name="trudnosc" value="8" />
-              <label for="trudnosc8"></label>
-              <input type="radio" id="trudnosc7" name="trudnosc" value="7" />
-              <label for="trudnosc7"></label>
-              <input type="radio" id="trudnosc6" name="trudnosc" value="6"  />
-              <label for="trudnosc6"></label>
-              <input type="radio" id="trudnosc5" name="trudnosc" value="5" />
-              <label for="trudnosc5"></label>
-              <input type="radio" id="trudnosc4" name="trudnosc" value="4" />
-              <label for="trudnosc4"></label>
-              <input type="radio" id="trudnosc3" name="trudnosc" value="3" />
-              <label for="trudnosc3"></label>
-              <input type="radio" id="trudnosc2" name="trudnosc" value="2"/>
-              <label for="trudnosc2"></label>
-              <input type="radio" id="trudnosc1" name="trudnosc" value="1" />
-              <label for="trudnosc1"></label>';
-            }
-
-
-            if($przepis->getStopienTrudnosci()==10){
-              echo '<input type="radio" id="trudnosc10" name="trudnosc" value="10" checked />
-              <label for="trudnosc10"></label>
-              <input type="radio" id="trudnosc9" name="trudnosc" value="9" />
-              <label for="trudnosc9"></label>
-              <input type="radio" id="trudnosc8" name="trudnosc" value="8" />
-              <label for="trudnosc8"></label>
-              <input type="radio" id="trudnosc7" name="trudnosc" value="7" />
-              <label for="trudnosc7"></label>
-              <input type="radio" id="trudnosc6" name="trudnosc" value="6"  />
-              <label for="trudnosc6"></label>
-              <input type="radio" id="trudnosc5" name="trudnosc" value="5" />
-              <label for="trudnosc5"></label>
-              <input type="radio" id="trudnosc4" name="trudnosc" value="4" />
-              <label for="trudnosc4"></label>
-              <input type="radio" id="trudnosc3" name="trudnosc" value="3" />
-              <label for="trudnosc3"></label>
-              <input type="radio" id="trudnosc2" name="trudnosc" value="2"/>
-              <label for="trudnosc2"></label>
-              <input type="radio" id="trudnosc1" name="trudnosc" value="1" />
-              <label for="trudnosc1"></label>';
-            }
-
             ?>
           </div>
         </div>
@@ -353,18 +123,6 @@
 
             </select>
           </div>
-          <!--Tutaj masz przykład użycia tych funkcji.
-          Po prostu je wywołuj w pętli dodając nowe Kategorie i zaznaczaj odpowiednie opcje/
-
-          Pierwszą kategorię zawsze powinnaś robić "ręcznie" jak wyżej, ale pozostałe
-          już musisz dodawać funkcją tak jak poniżej. Wybieranie również powinnaś funkcjami robić -->
-          <?php
-          //echo '<script type="text/javascript">addCategory()</script>';
-          //Pierwszy parametr to numer kategorii na stronie, drugi to numer opcji do zaznaczenia
-          //na liście. Numeruj zgodnie z kolejnościa wyświetlania na liście czyli Fit=0, Kolacja=1 itd.
-          //echo '<script type="text/javascript">CategorySelect(2, "Fit")</script>';
-          ?>
-
 
           <?php
           require_once __DIR__.'/vendor/autoload.php';
@@ -417,13 +175,7 @@
           <button type="submit" onmouseover="checkImages()"> Zmień przepis</button>
         </div>
         <script type="text/javascript" src="script-EdytowaniePrzepisu.js"></script>
-        <!-- PHP musi wywoływać funkcje po wcześniejszym
-        załączeniu tego mojego skryptu JS, bo inaczej
-        nie będzie ich widzieć.
 
-        Wszystkie etapy można dodać funkcją z javascripta, więc etapu 1 nie musisz robić ręcznie.
-        Składniki tak samo.
-        -->
         <?php
         require_once __DIR__.'/vendor/autoload.php';
         require_once __DIR__.'/generated-conf/config.php';
@@ -433,8 +185,6 @@
         if ($fp !== null) {
           echo '<script type="text/javascript">loadMainImageDB("'.base64_encode(stream_get_contents($fp)).'");</script>';
         }
-
-
 
         ///////////////////////////////////////////////////////////////wyswietlaja sie odpowiednie etapy (z bazy dla danego przepisu)
         $etapyOpis = EtapQuery::create()
@@ -519,16 +269,6 @@
 
   </main>
 
-
-
-
-
-  <!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-    crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
-    crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
-    crossorigin="anonymous"></script> -->
 </body>
 
 </html>
