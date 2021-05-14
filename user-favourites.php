f84212b2b09c6db7ba705c3c077d14a53bd66128
mwiech99 <80545933+mwiech99@users.noreply.github.com>
2021-05-14 15:54:04 +0200

    ulubione przepisy usera

--- user-favourites.php
+++ user-favourites.php
@@ -18,6 +18,82 @@ if(!isset($_SESSION['login'])) {
       <h1>Twoje ulubione przepisy</h1>
       <div class="content__elements" id="search-results">
 
+        <?php
+        require_once __DIR__.'/vendor/autoload.php';
+        require_once __DIR__.'/generated-conf/config.php';
+
+        $userLogin = $_SESSION['login'];
+
+
+        $pID = UlubioneQuery::create()
+          ->join('Uzytkownik')
+          ->join('Przepis')
+          ->where('Ulubione.UzytkownikLogin = ?', $userLogin)
+          ->select(array('Przepis.IdPrzepis'));
+          //->find();
+
+        $ilosc=0;
+        foreach($pID as $pid)
+        {
+          $ilosc++;
+        }
+        //$ileID = count($pID); //ilosc wszystkich przepisow w bazie
+        $ileID = $ilosc; //ilosc wszystkich przepisow w bazie
+        $rowsPerPage = 5; //ilosc przepisow wyswietlanych na jednej stronie
+        $totalPages = ceil($ileID / $rowsPerPage); //wyikowa ilosc wszsytkich stron
+
+        echo '</div>
+        <div class="content__search-counter" id="search-counter">';
+        $num=1;
+        for($num; $num<=$totalPages; $num++)
+        {
+          echo '<div class="content__search-counter__element"><a href="user.php?currentPage='.$num.'">'.$num.'</a></div>';
+        }
+        echo '</div>
+        <script type="text/javascript" src="script-WyszukiwaniePrzepisu.js"></script>';
+
+        if(isset($_GET['currentPage'])) {
+          $pageNumber = $_GET['currentPage'];
+        } else {
+        $pageNumber = 1;
+        }
+        $ulubionePrzepisyID = UlubioneQuery::create()
+                ->join('Uzytkownik')
+                ->join('Przepis')
+                ->where('Ulubione.UzytkownikLogin = ?', $userLogin)
+                ->select(array('Przepis.IdPrzepis'))
+                ->paginate($page = $pageNumber, $rowsPerPage = 5);
+        if(count($ulubionePrzepisyID)!==0)
+        {
+        foreach($ulubionePrzepisyID as $ID)
+        {
+          $lubieTo = Lubie_toQuery::create()
+                      ->filterByPrzepisIdPrzepis($ID)
+                      ->find();
+          $ileLike=0;
+          foreach($lubieTo as $l)
+          {
+            $ileLike++;
+          }
+
+          $pDane = PrzepisQuery::create()->findPk($ID);
+          $zdj = $pDane->getZdjecieOgolne();
+          if ($zdj !== null) {
+            echo '<script>addUserContentElement("'.$ID.'", "'.$pDane->getNazwa().'","'.$ileLike.'", "'.$pDane->getCzasPrzygotowania().'", "'.$pDane->getDlaIluOsob().'", "'.$pDane->getStopienTrudnosci().'", 1, "'.base64_encode(stream_get_contents($zdj)).'");</script>';
+          }
+          else{
+            echo '<script>addUserContentElement("'.$ID.'", "'.$pDane->getNazwa().'","'.$ileLike.'", "'.$pDane->getCzasPrzygotowania().'", "'.$pDane->getDlaIluOsob().'", "'.$pDane->getStopienTrudnosci().'", 0);</script>';
+          }
+        }
+        }
+        else
+        {
+        echo '<h2>Nie posiadasz jeszcze ulubionych przepisów!<i></i></h2></br></br></br></br></br>';
+        }
+        ?>
+
+
+
       </div>
     </section>
 
